b50a6a1f652c32b67f59bca865f90c3504c7560b
Moataz Ghazy <m.ghazy44@hotmail.com>
2024-02-23 11:17:07 -0500

    ensured post exists

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -2,8 +2,13 @@ import Posts from "@/utils/Models/Posts"
 import connectDB from "@/utils/db"
 import { notFound } from "next/navigation"
 
-export const metadata = {
-  title: "Post"
+export async function generateMetadata({ params: { postId } }) {
+  connectDB()
+  const post = await Posts.findOne({postId})
+  if (!post) notFound()
+  return {
+    title: post.title
+  }
 }
 
 export default async function Post({ params: { postId } }) {
