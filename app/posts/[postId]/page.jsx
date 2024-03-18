f2e5f5af27c136c0034edb3561f1c122a2953634
Moataz Ghazy <m.ghazy44@hotmail.com>
2024-03-18 10:21:41 -0400

    adapted for ssg

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,13 +1,24 @@
+import connectDB from "@/utils/db"
+import Posts from "@/utils/Models/Posts"
 import getPost from "@/utils/actions/get-post"
 import "react-quill/dist/quill.snow.css"
 import "@/app/styles/posts.css"
 import "@/app/styles/editor.css"
+
+export const revalidate = 10
 
 export async function generateMetadata({ params: { postId } }) {
   const post = await getPost(postId)
   return {
     title: post.title
   }
+}
+
+export async function generateStaticParams() {
+  await connectDB()
+  const posts = await Posts.find()
+  const postIds = posts.map(post => ({postId: post.postId}))
+  return postIds
 }
 
 export default async function Post({ params: { postId } }) {
